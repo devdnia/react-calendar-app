b157d1084ecd47896accb6f70bfc2dc1167a1ca6
Ivan <devdnia@gmail.com>
2022-07-12 20:09:19 +0200

    Variables de entorno

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -1,10 +1,12 @@
 
 export const getEnvVariables = () => {
 
-    import.meta.env;
+    // Solucionar problemas de yarn build
+    // import.meta.env;
     
     return {
-        ...import.meta.env,
+        // ...import.meta.env,
+        VITE_API_URL: import.meta.env.VITE_API_URL,
     }
 
 }
